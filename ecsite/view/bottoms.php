7ec966bc257fd092cfdd29e441cbd0d1dfa40b18
s-shenyu <shi_shenyu@outlook.com>
2022-10-24 09:58:23 +0900

    Create sample

--- ecsite/view/bottoms.php
+++ ecsite/view/bottoms.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home|ecsite</title>
+    <title>Bottoms|ecsite</title>
 </head>
 <body>
-    <p>文字コードは必ずUTF-8にしてください</p>
-    <p>bootstrapとVue.jsを活用して、見た目を作りましょう</p>
+    
 </body>
 </html>
