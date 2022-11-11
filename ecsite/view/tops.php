b3ab2ac96c08601214705f063284f9cd13d70ace
lvilvilvilvi <115960659+lvilvilvilvi@users.noreply.github.com>
2022-11-11 12:21:05 +0900

    shohin

--- ecsite/view/tops.php
+++ ecsite/view/tops.php
@@ -1,26 +1,14 @@
 <!DOCTYPE html>
-<html lang="jp">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tops|ecsite</title>
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+  <title>OnLineShop</title>
     <style>
-        .bg-img{
-  		background-image: url("../public/img_site/topimage.jpg");
-  		background-size: cover;
-  		background-position: center 60%;
-  	}
-
-    .iro {
-      color: gray;
-      text-decoration: none;
-    }
-
-    .nav-item{
+      .nav-item{
       display: flex;
 	    margin: 0 auto;
-	    padding: 0 5%;  
+	    padding: 0 5%;
 	    width: 94%;
         }
 
@@ -32,19 +20,17 @@
       margin:0 1%;
       color:white;
     }
-
     </style>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
 </head>
-<body>
-<body background="img/watercolor_00395.jpeg">
 
+<body background="../img/watercolor_00395.jpeg">
 <div class="container-fluid"style="background-color: #000000;">
-  <div class="row">
+  <div class="row"　>
 
-    <nav class=".fixed-top navbar navbar-expand-md navbar-dark " aria-label="Fourth navbar example" >
+  <nav class=".fixed-top navbar navbar-expand-md navbar-dark " aria-label="Fourth navbar example" >
       <div class="container-fluid"style="background-color: #000000;">
         <a class="navbar-brand" href="#">
           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
@@ -104,14 +90,17 @@
         </ul>
       </div>
     </nav>
-    <div name="maindiv" class="container">
+  </div>
+</div>
+
+  <div name="maindiv"　class="container">
   	<div class="row  gy-5 gx-3 mt-1 mr-1 ml-1 mb-5">
 
   	<div class="col-6 col-md-3">
   		<div class="card" style="border-style:none;">
         <button>
         <a href="top1.html" class="img">
-      		<img src="img/outer.png" class="card-img-top" alt="...画像  " >
+      		<img src="img/outer.png" class="card-img-top" alt="..." >
       			<div class="card-body">
         </a>
               <h5 class="card-title">ウールジッパージャケット</h5>
@@ -123,8 +112,61 @@
   		</div>
   	</div>
 
+  	<div class="col-6 col-md-3">
+  		<div class="card" style="border-style:none;">
+        <button>
+          <a href="top1.html">
+        		<img src="img/knit.png" class="card-img-top" alt="..."　style="border:none">
+        			<div class="card-body">
+          </a>
+      				<h5 class="card-title">クルーネックセーター</h5>
+              <div class="iro">
+                <p class="card-text">¥56,100</p>
+              </div>
+      			  </div>
+          </button>
+      	</div>
+  	 </div>
+
+  	<div class="col-6 col-md-3">
+  		<div class="card" style="border-style:none;">
+        <button>
+          <a href="top1.html">
+      		<img src="img/denim.png" class="card-img-top" alt="...">
+      			<div class="card-body">
+          </a>
+      				<h5 class="card-title">ルーズフィットジーンズ</h5>
+              <div class="iro">
+      				 <p class="card-text">¥48,950</p>
+              </div>
+      			</div>
+        </button>
+  		</div>
+  	</div>
+
+    <div class="col-6 col-md-3">
+      <div class="card" style="border-style:none;">
+        <button>
+          <a href="top1.html">
+            <img src="img/sho.png" class="card-img-top" alt="...">
+              <div class="card-body">
+          </a>
+                <h5 class="card-title">ハイカットスニーカー</h5>
+                <div class="iro">
+                  <p class="card-text">¥41,800</p>
+                </div>
+              </div>
+        </button>
+      </div>
+    </div>
+
+      
+
+  	</div>
   </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-</body>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+  </body>
+
 </html>
