38cb841ca94da3f0802f5d940f43fabe3d2527c6
lvilvilvilvi <115960659+lvilvilvilvi@users.noreply.github.com>
2022-11-19 19:59:42 +0900

    a

--- ecsite/view/buyCheck.php
+++ ecsite/view/buyCheck.php
@@ -49,8 +49,8 @@
       margin:50px;
     }
     
-    .aso{
-      background-color:red;
+    .gokei{
+      padding-left: 74%;
     }
     </style>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -150,14 +150,14 @@
     <div class="col-md-10 offset-md-1 mt-5 mb-5 pb-3" style="background-color:#FFFF;" >
       
 
-        <h3 style="padding-top: 30px; padding-bottom:50px; padding-left:30px; text-align:left">購入アイテム</h3>
+        <h3 style="padding-top: 2%; padding-bottom:4%; padding-left:2%; text-align:left">購入アイテム</h3>
 
        <div class="row">
-          <div class="col-7 text-dark "><p style="position:relative;left:380px;">アイテム名</p></div>
-          <div class="col-5 text-dark"><p style="position:relative;left:230px;">小計(税込)</p></div>
+          <div class="col-7 text-dark "><p style="position:relative;left:65%;">アイテム名</p></div>
+          <div class="col-5 text-dark"><p style="position:relative;left:63%;">小計(税込)</p></div>
        </div>
 
-       <hr width="100%"/>
+       <hr width="100%">
 
        <div class="row mt-3">
         <div class="col-md-3">
@@ -171,35 +171,44 @@
           </div>
         </div>  
 
-        <div class="row mt-4">
-        <div class="col-md-2">
+        <div class="row mt-3">
+        <div class="col-md-3">
         <img src="../public/img_site/s.jpg" width="150" height="150">
         </div>
           <div class="col-md-6 text-dark ">
                 Embroidered MA-1 blouson "Black" [@zastin_tcp] <br>
           </div>
-          <div class="col-md-2 text-dark">
+          <div class="col-md-3 text-dark">
               <p class="text-center">¥29,800</p>
           </div>
-          <div class="col-md-2 text-dark">
-              <p class="text-center">20XX/XX/XX</p>
-          </div>
-        </div> 
+        </div>  
 
-        <div class="row mt-4">
-        <div class="col-md-2">
+        <div class="row mt-3">
+        <div class="col-md-3">
         <img src="../public/img_site/s.jpg" width="150" height="150">
         </div>
           <div class="col-md-6 text-dark ">
                 Embroidered MA-1 blouson "Black" [@zastin_tcp] <br>
           </div>
-          <div class="col-md-2 text-dark">
+          <div class="col-md-3 text-dark">
               <p class="text-center">¥29,800</p>
-          </div>
-          <div class="col-md-2 text-dark">
-              <p class="text-center">20XX/XX/XX</p>
-          </div>
-        </div> 
+          </div> 
+        </div>  
+      <hr>
+      <h4 class="gokei">合計(税込) :￥123,000</h4>
+      <h3 style="padding-top: 2%; padding-bottom:1%; padding-left:2%; text-align:left">購入者情報</h3>
+      <hr>
+      <h5>お名前</h5>
+      <hr>
+      <h5>お届け先</h5>
+      <hr>
+      <h5>電話番号</h5>
+      <hr>
+      <h5>お支払方法</h5>
+      <hr>
+      <div align="center">
+      <button class=" btn btn-primary"style="width: 20%;">注文する</button>
+      </div>
 
       </div>
     </div>
