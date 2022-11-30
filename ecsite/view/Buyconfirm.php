9f50205efe82878a894aa65862cf0dce074ddba3
Satoyuta20 <115960280+Satoyuta20@users.noreply.github.com>
2022-11-30 10:32:03 +0900

    c

--- ecsite/view/Buyconfirm.php
+++ ecsite/view/Buyconfirm.php
@@ -46,6 +46,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+
 </head>
 
 <body background="../public/img_site/topimage.jpg">
@@ -112,6 +113,7 @@ echo $ken_data;
             <h4 class="font border-bottom"></h4>
 
             <h4 style="padding-left: 30px;">市区町村<br><input type="text" name="addres"></h4>
+            
             <h4 class="font border-bottom"></h4>
 
             <h4 style="padding-left: 30px;">番地以降<br><input type="text" name="ban"></h4>
