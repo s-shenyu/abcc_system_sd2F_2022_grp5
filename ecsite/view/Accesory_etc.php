3926e250f16c0dbb690bcf40167fb35027a7b6ad
lvilvilvilvi <115960659+lvilvilvilvi@users.noreply.github.com>
2022-11-30 12:03:24 +0900

    a

--- ecsite/view/Accesory_etc.php
+++ ecsite/view/Accesory_etc.php
@@ -1,4 +1,9 @@
- <!DOCTYPE html>
+<?php
+session_start();
+require '../model/DBManagermst.php';
+$dbmng = new DBManagermst();
+?>
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -26,10 +31,8 @@
             <div class="row  gy-5 gx-3 mt-1 mb-5">
 
             <?php
-              require '../model/DBManagermst.php';
-              $dbmng = new DBManagermst();
-              $result = $dbmng->showGoodsByTag('Acce');
               try {
+                $result = $dbmng->showGoodsByTag('Acce');
                 foreach ($result as $row) {
                   echo '<div class="col-6 col-md-3">';
                   echo '<div class="card" style="border-style:none;">';
